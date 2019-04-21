68cea81d1221f497c432c1de8f545a7927b7ea4f
Fajar <fajar.rizki@webimp.com.sg>
2019-04-21 18:45:10 +0700

    added billing and clinet stuuf

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,4 @@
 // keys.js - figure out what set of credentials to return
-// client ID
-// 162820990138-add5oicq7ie9f4uejeesc6dm0t1rjfrg.apps.googleusercontent.com
-// client secret
-// l3NzgUMYdOWP24Bz_od181Iy
 
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 // we use cookie-session instead of express-session
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -17,6 +18,7 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         // 30 days * 24 hours * 60 minutes * 60 seconds * 1000 miliseconds
@@ -28,7 +30,21 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+require('./routes/billingRoutes')(app);
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // Like our main.js or main.css file
+    app.use(express.static('client/build'));
+
+    // Express serve up the index.html file
+    // if it doesnt recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
